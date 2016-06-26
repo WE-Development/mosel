81c4cec97c6bf3974a98ca0f545577ebaaf7eb7b
robin <engel.robin@gmx.de>
2016-06-26 19:21:52 +0200

    playing with environmentvariable

--- moselui/src/app/moselui.component.ts
+++ moselui/src/app/moselui.component.ts
@@ -1,3 +1,4 @@
+import {environment} from "./";
 import {Component} from "@angular/core";
 import {CHART_DIRECTIVES} from "angular2-highcharts/index";
 
@@ -11,11 +12,12 @@ import {CHART_DIRECTIVES} from "angular2-highcharts/index";
 export class MoseluiAppComponent {
   constructor() {
     this.options = {
-      title : { text : 'simple chart' },
+      title: {text: environment.test},
       series: [{
         data: [29.9, 71.5, 106.4, 129.2],
       }]
     };
   }
-  options: Object;
+
+  options:Object;
 }
