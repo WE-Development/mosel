a842926234e4d5dd7a2d9c43d700ea145084867d
robin <engel.robin@gmx.de>
2016-07-29 23:36:38 +0200

    saver build process

--- moselui/scripts/build.js
+++ moselui/scripts/build.js
@@ -1,6 +1,23 @@
 var fs = require("fs");
 var browserify = require("browserify");
 var bablify = require("babelify");
+var cp = require("copyfiles");
+var rm = require("del");
+var mkdir = require("mkdirp");
+
+//cleanup
+rm.sync(['dist']);
+mkdir.sync('dist');
+
+cp([
+        './src/**/*.html',
+
+        './dist'
+    ], true,
+    function (err, files) {
+        if (typeof err != 'undefined') console.error(err);
+        console.log('Copied ' + files);
+    });
 
 browserify('src/MoselUI.es6')
     .transform(bablify, {presets: ["es2015"]})
