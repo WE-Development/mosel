ddebc15a57f448cc833c1d5b27ed41569f339795
robin <engel.robin@gmx.de>
2017-03-23 22:19:18 +0100

    mongo persistence compiles for first time

--- moseld/server/context/mongo_dataPersistence.go
+++ moseld/server/context/mongo_dataPersistence.go
@@ -19,11 +19,33 @@ import (
 	"gopkg.in/mgo.v2"
 	"time"
 	"github.com/bluedevel/mosel/api"
+	"gopkg.in/mgo.v2/bson"
+	"log"
 )
 
+type nodeDoc struct {
+	Name     string `bson:"name"`
+	Diagrams map[string]diagramDoc `bson:"diagrams"`
+}
+
+type diagramDoc struct {
+	Name   string `bson:"name"`
+	Graphs map[string]graphDoc `bson:"graphs"`
+}
+
+type graphDoc struct {
+	Name       string `bson:"name"`
+	DataPoints []dataPointDoc `bson:"dataPoints"`
+}
+
+type dataPointDoc struct {
+	Time  time.Time `bson:"time"`
+	Value string `bson:"value"`
+}
+
 type mongoDataPersistence struct {
-	session       *mgo.Session
-	serverContext *MoseldServerContext
+	session  *mgo.Session
+	database *mgo.Database
 }
 
 func NewMongoDataPersistence(session *mgo.Session) *mongoDataPersistence {
@@ -33,11 +55,49 @@ func NewMongoDataPersistence(session *mgo.Session) *mongoDataPersistence {
 }
 
 func (pers *mongoDataPersistence) Init() error {
+	pers.database = pers.session.DB("")
 	return nil
 }
 
-func (pers *mongoDataPersistence) Add(node string, t time.Time, info api.NodeInfo) {
+func (pers *mongoDataPersistence) Add(nodeName string, t time.Time, info api.NodeInfo) {
+	coll := pers.database.C("nodes")
 
+	selector := bson.M{"name": nodeName}
+
+	var node nodeDoc
+	itr := coll.Find(selector).Iter()
+
+	doUpdate := itr.Next(&node)
+
+	if err := itr.Err(); err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	initNodeIfNil(nodeName, &node)
+
+	for diagramName, graphs := range info {
+		var diagram diagramDoc
+		initDiagramIfNil(diagramName, &node, &diagram)
+
+		for graphName, value := range graphs {
+			var graph graphDoc
+			initGraphIfNil(graphName, &diagram, &graph)
+
+			point := dataPointDoc{
+				Time:t,
+				Value:value,
+			}
+
+			graph.DataPoints = append(graph.DataPoints, point)
+		}
+	}
+
+	if doUpdate {
+		coll.Update(selector, node)
+	} else {
+		coll.Insert(node)
+	}
 }
 
 func (pers *mongoDataPersistence) GetAll() (DataCacheStorage, error) {
@@ -46,4 +106,30 @@ func (pers *mongoDataPersistence) GetAll() (DataCacheStorage, error) {
 
 func (pers *mongoDataPersistence) GetAllSince(since time.Duration) (DataCacheStorage, error) {
 	return nil, nil
+}
+
+func initNodeIfNil(name string, node *nodeDoc) {
+	if node.Diagrams == nil {
+		node.Diagrams = make(map[string]diagramDoc)
+	}
+}
+
+func initDiagramIfNil(name string, node *nodeDoc, dia *diagramDoc) {
+	if _, ok := node.Diagrams[name]; !ok {
+		dia.Name = name
+		dia.Graphs = make(map[string]graphDoc)
+		node.Diagrams[name] = *dia
+	} else {
+		(*dia) = node.Diagrams[name]
+	}
+}
+
+func initGraphIfNil(name string, dia *diagramDoc, gr *graphDoc) {
+	if _, ok := dia.Graphs[name]; !ok {
+		gr.Name = name
+		gr.DataPoints = make([]dataPointDoc, 0)
+		dia.Graphs[name] = *gr
+	} else {
+		(*gr) = dia.Graphs[name]
+	}
 }
